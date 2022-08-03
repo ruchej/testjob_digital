0cb46b2ab404d4a9c8d12e4a7fe93e63634f69a5
Aleksandr Vinogradov <lvar-8@ya.ru>
2022-08-03 10:21:22 +0300

    Добавил менедж команду для генерации данных

---
+++ indicators/core/management/commands/adddata.py
@@ -0,0 +1,46 @@
+import datetime
+import random
+
+from django.db import transaction
+from django.contrib.auth import get_user_model
+from django.core.management import BaseCommand
+from core.models import Indicator
+
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = 'Создаёт тестовые данные'
+
+    def handle(self, *args, **options):
+
+        users = []
+        for u in User.objects.all():
+            if u.username != 'admin':
+                users.append(u)
+
+        if not users:
+            raise User.DoesNotExist('Пользователей в системе нет')
+
+        start_date = datetime.date(2022, 1, 1)
+        end_date = datetime.date(2022, 12, 30)
+        time_between_dates = end_date - start_date
+        days_between_dates = time_between_dates.days
+
+        with transaction.atomic():
+            for i in range(100):
+                user = random.choice(users)
+                random_number_of_days = random.randrange(days_between_dates)
+                random_date = start_date + datetime.timedelta(days=random_number_of_days)
+
+                Indicator.objects.create(
+                    fe=round(random.uniform(0, 20.0), 2),
+                    si=round(random.uniform(0, 20.0), 2),
+                    al=round(random.uniform(0, 20.0), 2),
+                    ca=round(random.uniform(0, 20.0), 2),
+                    s=round(random.uniform(0, 20.0), 2),
+                    user=user,
+                    date=random_date
+                )
+
--- indicators/core/models.py
+++ indicators/core/models.py
@@ -15,7 +15,7 @@ class Indicator(models.Model):
     al = models.FloatField(default=0, verbose_name='Алюминий')
     ca = models.FloatField(default=0, verbose_name='Кальций')
     s = models.FloatField(default=0, verbose_name='Сера')
-    date = models.DateField(auto_now_add=True, verbose_name='Дата добавления данных')
+    date = models.DateField(verbose_name='Дата добавления данных')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Сотрудник')
 
     def __str__(self):
