05ab4feb8fd244d81a6d24387fb948bee11ca30b
Aleksandr Vinogradov <lvar-8@ya.ru>
2022-08-06 20:55:14 +0300

    Добавлен запуск сборки и описание.

--- app/indicators/settings.py
+++ app/indicators/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', 'False').lower() == 'true'
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
 
